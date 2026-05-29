7a436ddf3c32ba312b1d67221393034923d21a69
Demis <darmfma@gmail.com>
2026-05-29 11:39:00 -0600

    feat: initialize project configuration and add dynamic login page component

--- Hydra-Admin/app/app/(auth)/login/login-content.tsx
+++ Hydra-Admin/app/app/(auth)/login/login-content.tsx
@@ -1,4 +1,13 @@
-import { LoginPageContentInner } from './components/LoginPageContentInner';
+'use client';
+import dynamic from 'next/dynamic';
+
+const LoginPageContentInner = dynamic(
+  () =>
+    import('./components/LoginPageContentInner').then((m) => ({
+      default: m.LoginPageContentInner,
+    })),
+  { ssr: false },
+);
 
 export function LoginPageContent() {
   return <LoginPageContentInner />;
